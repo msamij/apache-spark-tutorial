615fb4ccb496377d16a2b46efd69643b22666470
Muhammad Sami <muhammadsami290@outlook.com>
2024-04-23 14:24:57 +0500

    Spark is lazy!

---
+++ net.jgp.books.spark.ch04/src/main/java/net/jgp/books/spark/ch04/lab200_transformation_and_action/scripts/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+args="noop"
+class_name="net.jgp.books.spark.ch04.lab200_transformation_and_action.TransformationAndActionApp"
+jar_path="/home/muhammadsami/Public/Projects/Spark/net.jgp.books.spark.ch04/target/spark-in-action2-chapter04-1.0.0-SNAPSHOT.jar"
+
+# Check if the Maven build was successful
+if [ $? -eq 0 ]; then
+    # Run spark-submit with the specified class and JAR file
+    spark-submit --class "$class_name" "$jar_path" "$args"
+else
+    echo "Maven package failed. Exiting..."
+    exit 1
+fi
---
+++ net.jgp.books.spark.ch04/src/main/java/net/jgp/books/spark/ch04/lab900_misc/HelloWorldApp.java
@@ -0,0 +1,23 @@
+package net.jgp.books.spark.ch04.lab900_misc;
+
+/**
+ * Hello world.
+ */
+public class HelloWorldApp {
+
+  /**
+   * main() is your entry point to the application, let's display 50,000
+   * hello world: it takes about the same time as creating 7.5m datapoints
+   * using Spark, just for the fun of it...
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+    long t0 = System.currentTimeMillis();
+    for (int i = 0; i < 50000; i++) {
+      System.out.println("Hello, world");
+    }
+    long t1 = System.currentTimeMillis();
+    System.out.println("1. Hello world ... " + (t1 - t0));
+  }
+}
